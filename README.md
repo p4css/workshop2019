fc5dc82b4f2c8bc84e51c6653e20fcd274e43849
jirlong <jirlong@gmail.com>
2019-11-04 11:01:16 +0800

    20191104

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# workshop2019
+# Workshop2019
+- 講者: 謝吉隆、許家瑜、吳冠賢
+- 助教: 曾子軒、邱怡萱、連俊翔
+
+
+# 分析案例
+- [Twitter Safety(2019-08-19). Information operations directed at Hong Kong](https://blog.twitter.com/en_us/topics/company/2019/information_operations_directed_at_Hong_Kong.html)
+- [蘋果日報（2019-08-21）。港大傅景華：反送中燃起國際信息戰狼煙](https://tw.appledaily.com/new/realtime/20190821/1620095/)
+- [READr（2019-08-22）。Twitter 大戰中國網軍！？解密被刪帳號資料集](https://www.readr.tw/post/2013)
+
+# 行前通知
+1. 請先從右上角下載本github repo，並解壓縮至個人電腦。
+2. 下載以下資料集（[Deleted accounts and tweets]()），並將資料集內的檔案拖至前步驟資料夾中的data資料夾內。
+3. 依照影片內容設定環境
+4. 依照影片內容了解Ｒ的基本使用
+
+
+# Workshop slides
+
+
+# 相關程式碼
+```
+detects <- "港警|逃犯條例|反修例|遊行|修例|反送中|anti-extradition|hongkong|hkpolicebrutality|soshk|hongkongprotesters|HongKongPolice|hkpoliceforce|freedomHK|antiELAB|HongKongProtests|antiextraditionlaw|HongKongProtest|七一|游行|民阵|HongKong|逃犯条例|民陣|撐警|香港眾志|HongKongProterst"
+```
  
---
+++ W0_install_pkgs.R
@@ -0,0 +1,22 @@
+# packages for workshop 
+# tidyverse is a collection of useful packages which match the dplyr coding style
+# The core tidyverse includs
+#       dplyr for data manipulation grammar, e.g., select(), filter(), mutate()
+#       ggplot2 for visualization
+#       lubridate for manipulating date and time objects, e.g., year()
+#       stringr for string operations, e.g., str_sub()
+#       readr for file reading, e.g., read_csv()
+# jiebaR for chinese word tokenization
+# rtweet is a convinient R client to access twitter data
+# tidytext for processing
+# igraph for network analysis and visualization
+# bbplot, built by bbc team, defines publising format of R, 
+# bbplot, see https://bbc.github.io/rcookbook/
+
+pkgs <- c("tidyverse", "jiebaR", "rtweet", "tidytext", "igraph", "bbplot")
+
+# installing packages not in the computer
+pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])] 
+if(length(pkgs)) install.packages(pkgs)
+
+
